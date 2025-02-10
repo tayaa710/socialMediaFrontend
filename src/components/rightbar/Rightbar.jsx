5cfc7e7a9ed2a2b0a96abf354c1991616999ab6b
tayaa710 <154112253+tayaa710@users.noreply.github.com>
2025-02-10 20:53:01 +1300

    Added feed, sidebar and rightbar and then made designed sidebar

---
+++ src/components/rightbar/Rightbar.jsx
@@ -0,0 +1,10 @@
+import './rightbar.css'
+
+const Rightbar = () => {
+  return (
+    <div className="rightbar">rightbar</div>
+    
+  )
+}
+
+export default Rightbar
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,21 @@
 import Topbar from "../../components/topbar/Topbar"
+import Sidebar from "../../components/sidebar/Sidebar"
+import Feed from "../../components/feed/Feed"
+import Rightbar from "../../components/rightbar/rightbar"
+import "./home.css"
+
 
 const Home = () => {
   return (
-  <div>
+  <>
     <Topbar/>
-  </div>
+    <div className="homeContainer">
+    <Sidebar/>
+    <Feed/>
+    <Rightbar/>
+    </div>
+    
+  </>
 )
 }
 
