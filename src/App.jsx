b99e4a0f0a74b2bc6c6c5fdde3d12cd89228a693
tayaa710 <154112253+tayaa710@users.noreply.github.com>
2025-02-12 21:09:22 +1300

    finished frontend initial design

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Home from "./pages/home/Home"
 import { Person } from "@mui/icons-material"
 import Profile from "./pages/profile/Profile"
+import Login from "./components/login/Login"
+import Register from "./components/register/Register"
 
 function App() {
 
   return (
     <div>
-      <Profile />
+      <Home />
     </div>
   )
 }
--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -9,7 +9,7 @@ const Rightbar = ({ profile }) => {
   return (
     <div className="rightbar">
       <div className="rightbarWrapper">
-        <ProfileRightbar />
+        {profile ? <ProfileRightbar /> : <HomeRightbar />}
       </div>
     </div>
 
